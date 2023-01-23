12b57f92fe1fa713deab189ce80ec81024188b90
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2023-01-23 14:42:50 -0300

    Configuração dos routes do express

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const loginRouter = require('./routes/login.router');
 
 const app = express();
+
+app.use(loginRouter);
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
