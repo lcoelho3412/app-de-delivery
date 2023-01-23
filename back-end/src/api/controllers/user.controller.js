092c276d57b03f2930032e033ecd7e71b9f2f44f
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2023-01-23 19:59:15 -0300

    Atualização da rota

--- back-end/src/api/controllers/user.controller.js
+++ back-end/src/api/controllers/user.controller.js
@@ -1,8 +1,6 @@
 const userService = require('../services/user.service');
 
 const createUser = async (req, res) => {
-  // const { name, email, password, role } = req.body;
-
   const newUser = await userService.createUser(req.body);
 
   res.status(201).json(newUser);
