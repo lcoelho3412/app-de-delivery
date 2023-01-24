56206fea353e30d2a1a9d26d026d2b1d80f29c1d
Rodrigo Frangel <soumuitorodrigo@gmail.com>
2023-01-24 18:33:19 -0300

    fix: correção na importação

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const port = process.env.PORT || 3001;
 const app = require('./app');
 
-const userRouter = require('./routes/resgister.router');
+const userRouter = require('./routes/register.routes');
 
 app.listen(port);
 
