791915f30de129b6c6a8b06b09cda928a7e52e9b
Sally Diament <sally.barzilai@gmail.com>
2023-01-24 15:48:16 -0300

    iniciando os testes da rota login no BE

---
+++ back-end/src/tests/login.integration.test.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
+const app = require('../../src/app');
+const connection = require('../../src/db/connection');
+
+const { expect, use } = chai;
+
+use(chaiHttp);
