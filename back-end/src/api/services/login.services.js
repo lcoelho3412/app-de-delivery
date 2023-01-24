18e7608d5ad05aeb41557ec4668b232c44766b70
Sally Diament <sally.barzilai@gmail.com>
2023-01-23 21:31:55 -0300

    concluído o backend de login

--- back-end/src/api/services/login.services.js
+++ back-end/src/api/services/login.services.js
@@ -1,27 +1,34 @@
-const bcrypt = require('bcryptjs');
+const md5 = require('md5');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
 const { User } = require('../../database/models');
-const createToken = require('../utils/jwt.utils');
+
+const JWT_SECRET = fs.readFileSync(path.resolve('jwt.evaluation.key'));
+
+const createToken = (data) => {
+    const token = jwt.sign({ data }, JWT_SECRET, {
+        expiresIn: '1d',
+        algorithm: 'HS256', 
+    });
+
+    return token;
+};
 
 const validateLogin = async ({ email, password }) => {
     if (!email || !password) throw new Error('Invalid login');
     const user = await User.findOne({ 
         attributes: ['id', 'email', 'name', 'password', 'role'],
         where: { email } });
-    console.log('password do bd:', user.password);
-    console.log('password do body:', password);
 
     if (!user) throw new Error('User not found.');
-  
-    const match = await bcrypt.compare(password, user.password);
-    console.log(await bcrypt.compare(password, user.password));
-    
-    if (!match) throw new Error('Wrong password.');
 
-console.log('resultado da bcrypt:', match);
+    const hashedPassword = md5(password);
+    if (hashedPassword !== user.password) throw new Error('Wrong password.');
+
     const { password: _, ...userWithoutPassword } = user.dataValues;
 
     const token = createToken(userWithoutPassword);
-console.log('token na service:', token);
     return { token };
 };
 
