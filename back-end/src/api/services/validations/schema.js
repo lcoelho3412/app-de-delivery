61d7b45b1724e945a77eb04eebc62107e156c1b9
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-25 00:28:43 -0300

    feat: schema Joi para login e register

---
+++ back-end/src/api/services/validations/schema.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const registerSchema = Joi.object({
+  name: Joi.string().min(12).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+module.exports = { loginSchema, registerSchema };
