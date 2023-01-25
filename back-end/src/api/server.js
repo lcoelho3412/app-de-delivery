c58b1e4b7421a3e4179ceb2b850c807f684e55a4
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-25 14:43:13 -0300

    Update server.js

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -3,10 +3,6 @@ require('dotenv').config();
 const port = process.env.PORT || 3001;
 const app = require('./app');
 
-const userRouter = require('./routes/register.routes');
-
 app.listen(port);
-
-app.use(userRouter);
 
 console.log(`Api rodando na porta ${port}`);
