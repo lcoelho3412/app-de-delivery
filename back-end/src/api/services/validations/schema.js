b1cf80cdd9758e19aaea947abd219e1b05a0bcac
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-25 13:26:11 -0300

    fix: name com maximo de 12 caracteres

--- back-end/src/api/services/validations/schema.js
+++ back-end/src/api/services/validations/schema.js
@@ -6,7 +6,7 @@ const loginSchema = Joi.object({
 });
 
 const registerSchema = Joi.object({
-  name: Joi.string().min(3).required(),
+  name: Joi.string().max(12).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
