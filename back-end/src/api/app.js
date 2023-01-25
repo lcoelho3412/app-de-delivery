1b254b69f7e216e7d414a7d80c331b14d7b5d7ef
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-24 21:28:10 -0300

    feat: express-async-errors para erros genericos

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
+require('express-async-errors');
 const cors = require('cors');
 const router = require('./routes');
-
-const ErrorHandler = require('./middlewares/errorHandler');
+const httpErrorMiddleware = require('./middlewares/http.error.middleware');
 
 const app = express();
 
@@ -14,6 +14,6 @@ app.use('/login', router.login);
 
 app.use('/register', router.register);
 
-app.use(ErrorHandler.handle);
+app.use(httpErrorMiddleware);
 
 module.exports = app;
