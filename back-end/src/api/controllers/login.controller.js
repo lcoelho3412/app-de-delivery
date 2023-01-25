7f7f8a567de412e3c44ec53a9ff4898f0e2cd683
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-25 00:04:42 -0300

    refactor: remove try catch e add Joi para validar body

---
+++ back-end/src/api/controllers/login.controller.js
@@ -0,0 +1,15 @@
+const service = require('../services');
+const validateSchema = require('../services/validations/validationSchema');
+const { loginSchema } = require('../services/validations/schema');
+
+const signIn = async (req, res) => {
+  const { body } = req;
+
+  await validateSchema(loginSchema, body);
+
+  const token = await service.login.signIn(body);
+
+  return res.status(200).json({ token });
+};
+
+module.exports = { signIn };
