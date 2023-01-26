297c573d5cb4f19dd13b9deab50442c335c1688b
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-26 19:31:55 -0300

    add:  import e export de sale e products

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,9 +1,11 @@
 const login = require('./login.routes');
 const register = require('./register.routes');
 const products = require('./products.routes');
+const sales = require('./sales.routes');
 
 module.exports = {
   login,
   register,
   products,
+  sales,
 };
