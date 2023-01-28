ea95937b24e0b6b17e8fe68b10d6a52506632f5a
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2023-01-27 21:29:46 -0300

    Requisito 17

--- front-end/src/routes/index.js
+++ front-end/src/routes/index.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Login, NotFound, Registro, Products } from '../pages';
+import Checkout from '../pages/Checkout';
 
 export default function Routes() {
   return (
@@ -9,6 +10,7 @@ export default function Routes() {
         <Route exact path="/login" component={ Login } />
         <Route exact path="/register" component={ Registro } />
         <Route exact path="/customer/products" component={ Products } />
+        <Route exact path="/customer/checkout" component={ Checkout } />
 
         <Route path="*" component={ NotFound } />
       </Switch>
