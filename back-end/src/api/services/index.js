50f16bdb4ea2a5476ed8af25d6285f4b641d06ec
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-29 20:03:30 -0300

    add: import e export de admin

--- back-end/src/api/services/index.js
+++ back-end/src/api/services/index.js
@@ -2,10 +2,12 @@ const login = require('./login.service');
 const register = require('./register.service');
 const products = require('./products.service');
 const sales = require('./sales.service');
+const admin = require('./admin.service');
 
 module.exports = {
   login,
   register,
   products,
   sales,
+  admin,
 };
