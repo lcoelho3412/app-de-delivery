b72bcfaa760ae162fa4af2d67ffb07f61611c949
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-29 20:18:01 -0300

    feat: service de admin lista e deleta usuarios

--- back-end/src/api/services/admin.service.js
+++ back-end/src/api/services/admin.service.js
@@ -1,54 +1,28 @@
-const md5 = require('md5');
 const { User } = require('../../database/models');
 const httpException = require('../utils/http.exception');
 
-const notAdmin = 'Você não é admin';
-
-const createUser = async (body, role) => {
-  if (role !== 'administrator') throw httpException(409, notAdmin);
-  
-  const { email, password } = body;
-
-  const hashedPassword = md5(password);
-
-  const user = await User.findOne({
-    where: { email },
-  });
-
-  if (user) throw httpException(409, 'Email já cadastrado');
-
-  await User.create({
-    ...body,
-    password: hashedPassword,
-  });
-
-  const newUser = await User.findOne({ where: { email } });
-
-  const { id, ...nUser } = newUser.dataValues;
-
-  return nUser;
-};
-
-const findUser = async (role) => {
-  if (role !== 'administrator') throw httpException(409, notAdmin);
-
+const findAll = async () => {
   const users = await User.findAll({});
 
-  if (!users) throw httpException(409, 'Nenhum usuário cadastrado');
-console.log(users);
   return users;
 };
 
-const deleteUser = async (body, role) => {
-  if (role !== 'administrator') throw httpException(409, notAdmin);
-  
-  const { email } = body;
-
-  await User.deleteOne({
-    where: { email },
+const findById = async (id) => {
+  const user = await User.findOne({
+    where: { id },
   });
 
-  // return nUser;
+  if (!user) throw httpException(409, 'Nenhum usuário cadastrado');
+
+  return user;
 };
 
-module.exports = { createUser, findUser, deleteUser };
+const remove = async (id) => {
+  await findById(id);
+
+  await User.destroy({
+    where: { id },
+  });
+};
+
+module.exports = { findAll, findById, remove };
