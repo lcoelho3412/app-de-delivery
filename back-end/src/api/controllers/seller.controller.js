6f8bdc6cf4afbd44d8a6bf41a921fa6effa35ea8
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-31 20:51:00 -0300

    refactor: pega token e traz apenas seu email

--- back-end/src/api/controllers/seller.controller.js
+++ back-end/src/api/controllers/seller.controller.js
@@ -1,11 +1,18 @@
+const { decode } = require('jsonwebtoken');
 const service = require('../services');
 
 const getOrdersBySeller = async (req, res) => {
-const { email } = req.body;
-const ordersBySeller = await service.seller.getOrdersBySeller(email);
-return res.status(200).json(ordersBySeller);
+  const { authorization } = req.headers;
+
+  const {
+    data: { email },
+  } = decode(authorization);
+
+  const ordersBySeller = await service.seller.getOrdersBySeller(email);
+
+  return res.status(200).json(ordersBySeller);
 };
 
 module.exports = {
-    getOrdersBySeller,
+  getOrdersBySeller,
 };
