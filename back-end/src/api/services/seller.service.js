682b69b21d69e96dc680ea620f00c3615f418684
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-01-31 21:06:27 -0300

    refactor: remoção de try catch

--- back-end/src/api/services/seller.service.js
+++ back-end/src/api/services/seller.service.js
@@ -1,24 +1,29 @@
 const { User, Sale } = require('../../database/models');
+// const httpException = require('../utils/http.exception');
 
 const getOrdersBySeller = async (email) => {
-try { 
-const seller = await User.findOne({
+  const seller = await User.findOne({
     where: {
       email,
       role: 'seller',
     },
   });
-const sellerId = Number(seller.dataValues.id);
-console.log('sellerId', sellerId);
-const orders = await Sale.findAll({ 
-    where: { sellerId }, 
-});
-return orders; 
-} catch (e) {
-    throw new Error(e);
-}
+
+  const sellerId = Number(seller.dataValues.id);
+
+  const sales = await Sale.findAll({
+    where: {
+      sellerId,
+    },
+  });
+
+  // if (sales.length === 0) {
+  //   throw httpException(404, 'Não há pedidos');
+  // }
+
+  return sales;
 };
 
 module.exports = {
-    getOrdersBySeller,
+  getOrdersBySeller,
 };
