64c6ef4144eba46175a90efb71af2d0e3f26ce6c
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-01 01:00:05 -0300

    feat: pagina de SellerOrdersDetails

---
+++ front-end/src/pages/Seller/SellerOrdersDetails.jsx
@@ -0,0 +1,10 @@
+import { NavBarSeller, SellerOrderCardDetails } from '../../components';
+
+export default function SellerOrdersDetails() {
+  return (
+    <>
+      <NavBarSeller />
+      <SellerOrderCardDetails />
+    </>
+  );
+}
