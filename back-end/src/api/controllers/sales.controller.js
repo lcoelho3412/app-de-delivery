c2417a8f38a52990a97a74c003000086d63241c7
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-02 16:06:08 -0300

    feat: controller de get para sales

--- back-end/src/api/controllers/sales.controller.js
+++ back-end/src/api/controllers/sales.controller.js
@@ -1,6 +1,8 @@
+const { decode } = require('jsonwebtoken');
 const service = require('../services');
 const validateSchema = require('../services/validations/validationSchema');
 const { saleSchema } = require('../services/validations/schema');
+const { validateToken } = require('../utils/jwt.util');
 
 const create = async (req, res) => {
   const { body } = req;
@@ -12,4 +14,18 @@ const create = async (req, res) => {
   return res.status(201).json(sale);
 };
 
-module.exports = { create };
+const salesByUser = async (req, res) => {
+  const { authorization } = req.headers;
+  
+  await validateToken(authorization);
+
+  const {
+    data: { email },
+  } = decode(authorization);
+
+  const sales = await service.sales.salesByUser(email);
+
+  return res.status(200).json(sales);
+};
+
+module.exports = { create, salesByUser };
